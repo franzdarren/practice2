353a878a9ef9bc53c6fab6b6adfada50e044d880
franzdarren <darren.domantay@awsys-i.com>
2026-09-21 23:48:43 +0800

    asd

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,9 +10,9 @@ function Home() {
   const [searchQuery, setSearchQuery] = useState("")
 
   const anime = [
-    { id: 1, title: "Love is war", year: 2026, description: "description sample", rating: 3 },
-    { id: 2, title: "Love is war2", year: 2023, description: "dawd sample", rating: 4 },
-    { id: 3, title: "Love is war", year: 1982, description: "a 2323", rating: 5 }
+    { id: 1, title: "Sample 1", year: 6767, description: "description sample", rating: 3.67 },
+    { id: 2, title: "Love is war", year: 2023, description: "dawd sample", rating: 4.99 },
+    { id: 3, title: "bruuH", year: 1982, description: "a 2323", rating: 5 }
   ]
 
   const handleSearch = (e) => {
