5dae1de0344a9bcc92a71fb9320f1b4e4823b2ab
franzdarren <darren.domantay@awsys-i.com>
2026-09-21 22:43:23 +0800

    upd

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,16 +3,30 @@ import '../components/AnimeCard.css'
 import AnimeCard from '../components/AnimeCard'
 
 function Home() {
-    const anime=[
-        {id:1, title: "Love is war", year: 2026, description: "description sample", rating: 3},
-        {id:2, title: "Love is war2", year: 2023, description: "dawd sample", rating: 4},
-        {id:3, title: "Love is war", year: 1982, description: "a 2323", rating: 5}
-    ]
+  const anime = [
+    { id: 1, title: "Love is war", year: 2026, description: "description sample", rating: 3 },
+    { id: 2, title: "Love is war2", year: 2023, description: "dawd sample", rating: 4 },
+    { id: 3, title: "Love is war", year: 1982, description: "a 2323", rating: 5 }
+  ]
+
+  const handleSearch = () => {
+    alert("hello im here")
+  }
   return (
-      anime.map((item)=>{
-        return(<AnimeCard key={item.id} title={item.title}  year={item.title} description={item.description} rating={item.rating}/>)
-      }
-  ))
+    <>
+      <div className='home'>
+        <form onSubmit={handleSearch} className="searchForm">
+          <input type="text" placeholder='Search for Anime...' className="search-input"></input>
+          <button type="submit" className="search-button">Search</button>
+        </form>
+      </div>
+      <div className="anime-card-list">
+        {anime.map((item) => {
+          return (<AnimeCard key={item.id} title={item.title} year={item.year} description={item.description} rating={item.rating} />)
+        })}
+      </div>
+    </>
+  )
 }
 
 export default Home;
